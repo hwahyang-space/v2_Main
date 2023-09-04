c4a42a0c03a413d65698edde9e047be8ecb97a09
HwaHyang <me@hwahyang.space>
2023-09-04 11:40:37 +0900

    feat: 404/403 redirect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
+import App, { AppContext } from 'next/app';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
 	const [mounted, setMounted] = useState<boolean>(false);
 
 	useEffect(() => {
@@ -10,3 +11,14 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return mounted && <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+	const appProps = await App.getInitialProps(appContext);
+
+	if (appContext.ctx.res?.statusCode === 403 || appContext.ctx.res?.statusCode === 404) {
+		appContext.ctx.res.writeHead(302, { Location: '/' });
+		appContext.ctx.res.end();
+	}
+
+	return { ...appProps };
+};
