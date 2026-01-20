2a7df423eaa0fb3b62c2d29b4354e79d9b71d5cf
HwaHyang (KangHyeon Kim) <hwahyang@meowlabs.kr>
2026-01-20 21:52:13 +0900

    chore: updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['cdn.meowlabs.kr'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.meowlabs.kr',
+			},
+		],
 	},
 };
 
